17e645e23b4277a6ee317e29f65a7f916269cc30
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-03-10 22:44:07 +0600

    report-post-is-ok-now

--- js/app.js
+++ js/app.js
@@ -152,7 +152,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  //console.log(reportedPosts);
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
